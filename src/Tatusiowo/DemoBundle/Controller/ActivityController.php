e55cb2472f492b9cd2941fd28e0315841f7b7827
galileo <galileox86@gmail.com>
2014-06-01 11:28:15 +0200

    Add improvements of list create details

--- src/Tatusiowo/DemoBundle/Controller/ActivityController.php
+++ src/Tatusiowo/DemoBundle/Controller/ActivityController.php
@@ -5,6 +5,7 @@ namespace Tatusiowo\DemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Tatusiowo\DemoBundle\Entity\Activity;
 
 class ActivityController extends Controller
 {
@@ -21,4 +22,32 @@ class ActivityController extends Controller
 
         return array('activities' => $activities);
     }
+
+    /**
+     * @param $activityId
+     *
+     * @Route("/{activityId}", name="tatusiowo_activity_detail")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Template()
+     */
+    public function detailAction($activityId)
+    {
+        $activity = $this->findOneOrFail($activityId);
+
+        return ['activity' => $activity];
+    }
+
+    /**
+     * @param $activityId
+     *
+     * @return Activity
+     */
+    private function findOneOrFail($activityId)
+    {
+        $activity = $this->get('tatusiowo_activity_repository')->find($activityId);
+
+        return $activity;
+    }
+
 }
